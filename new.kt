9d1d7c643ed8ebff5298c94ba4e4203117fc0cbc
Satyajit Panigrahi <satyapani999@gmail.com>
2020-06-12 12:51:17 +0530

    Add files via upload

---
+++ new.kt
@@ -0,0 +1,17 @@
+fun main()
+{
+	var ar=arrayOf<Int>(11,22,13,43,25,6)
+	for (i in (0..(ar.size)-1))
+		for (j in (0..i-1))
+			{
+				if(ar[i] < ar[j])
+					{
+						var t=ar[i]
+						ar[i]=ar[j]
+						ar[j]=t
+					}
+			}
+	println("sorted array is ")	
+	for (k in 0..ar.size)
+			println(ar[k])
+}
